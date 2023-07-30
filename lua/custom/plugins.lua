478279f5163a03d0ea68b32498dadf6fbd270b1a
Ydot19 <abdalhalim.yaadata@gmail.com>
2023-07-30 13:32:49 -0700

    feat: add harpoon

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -4,6 +4,28 @@ local plugins = {
     lazy = false,
   },
   {
+    "ThePrimeagen/harpoon",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    lazy = true,
+    keys = {
+      {"<leader>ma", function() require('harpoon.mark').add_file() end, {noremap = true}},
+      {"<leader>mc", function() require('harpoon.mark').clear_all() end, {noremap = true}},
+      {"<leader>md", function() require('harpoon.mark').rm_file() end, {noremap = true}},
+
+      {"<leader>mu", function() require('harpoon.ui').toggle_quick_menu() end, {noremap = true}},
+      {"<leader>ml", function() require('harpoon.ui').nav_next() end, {noremap = true}},
+      {"<leader>mh", function() require('harpoon.ui').nav_prev() end, {noremap = true}},
+
+      {"<leader>m1", function () require('harpoon.ui').nav_file(1) end, {noremap = true}},
+      {"<leader>m2", function () require('harpoon.ui').nav_file(2) end, {noremap = true}},
+      {"<leader>m3", function () require('harpoon.ui').nav_file(3) end, {noremap = true}},
+      {"<leader>m4", function () require('harpoon.ui').nav_file(4) end, {noremap = true}},
+      {"<leader>m5", function () require('harpoon.ui').nav_file(5) end, {noremap = true}},
+    }
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
@@ -20,7 +42,7 @@ local plugins = {
         "thrift",
         "regex",
 
-        -- tf 
+        -- tf
         "hcl",
         "terraform",
 
